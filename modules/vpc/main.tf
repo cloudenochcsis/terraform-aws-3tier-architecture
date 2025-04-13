ac135a08619939c8e1978f105edeb28630674c36
Cloud Enoch <cloudenochcsis@gmail.com>
2025-04-13 17:35:21 +0000

    Removed domain attribute from EIP for compatibility

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -58,7 +58,6 @@ resource "aws_subnet" "private_db" {
 
 # NAT Gateway
 resource "aws_eip" "nat" {
-  domain = "vpc"
   tags = merge(var.project_tags, {
     Name = "nat-eip"
   })
@@ -91,7 +90,7 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.main.id
   }
 
